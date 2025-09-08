c38f847c91ebfca178fe3a75db3abdc2bfa51ebd
vm4650894-debug <vm4650894@gmail.com>
2025-09-08 14:27:29 -0300

    Add files via upload

---
+++ licao.py
@@ -0,0 +1,14 @@
+idade= int (input('digite sua idade'))
+
+if idade <=12:
+    print ("criança")
+
+elif idade <=17:
+    print ("adolescente")   
+
+elif idade <=59:
+    print ("adulto") 
+
+else:
+    print ("idoso")
+
